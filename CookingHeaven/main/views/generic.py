76e956f33010474b13622e7af40bbff8dca40696
george-tenev <georgiev.georgi1999@gmail.com>
2022-04-01 16:01:22 +0300

    Add home page

--- CookingHeaven/main/views/generic.py
+++ CookingHeaven/main/views/generic.py
@@ -5,24 +5,22 @@ from CookingHeaven.accounts.models import Profile
 from CookingHeaven.main.models import Recipe
 
 
-class IndexTemplateView(views.TemplateView):
+class IndexTemplateView(views.ListView):
+    model = Recipe
     template_name = 'main/home.html'
+    context_object_name = 'recipes'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        recipes = Recipe.objects.all()
-        context['recipes'] = recipes
-        return context
-
-
+    def get_queryset(self):
+        queryset = Recipe.objects.order_by('-created_at')[:3]
+        return queryset
 
 class DashboardView(views.ListView):
     model = Recipe
     template_name = 'main/dashboard.html'
     context_object_name = 'recipes'
     paginate_by = 3
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super().get_context_data(*args, **kwargs)
-    #     recipes = Recipe.objects.all()
-    #     self.object_list = recipes
-    #     return context
+
+    def get_queryset(self):
+        queryset = Recipe.objects.order_by('-created_at')
+        return queryset
+
