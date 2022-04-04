18f9a0fe022f14d1b84f8f7f3a29ef22980521ad
george-tenev <georgiev.georgi1999@gmail.com>
2022-04-04 16:05:24 +0300

    Add fixes

--- CookingHeaven/main/views/category.py
+++ CookingHeaven/main/views/category.py
@@ -3,11 +3,11 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from CookingHeaven.common.view_mixins import AdminPermissionsRequiredMixin
-from CookingHeaven.main.models import FoodType
+from CookingHeaven.main.models import Category
 
 
-class FoodtypeCreateView(LoginRequiredMixin, AdminPermissionsRequiredMixin, CreateView):
-    model = FoodType
+class CategoryCreateView(LoginRequiredMixin, AdminPermissionsRequiredMixin, CreateView):
+    model = Category
     form_class = AdminPermissionsRequiredMixin
     template_name = 'main/product_create.html'
     success_url = reverse_lazy('dashboard')
